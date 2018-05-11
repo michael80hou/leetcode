332908aaf8c71a102835521ad70bd80e95ae1108
michael80hou <michael80hou@gmail.com>
2018-05-11 15:56:06 +0800

    optimize longest sub str

--- string/lengthOfLongestSubstring.c
+++ string/lengthOfLongestSubstring.c
@@ -3,48 +3,54 @@
 #include <assert.h>
 #include <string.h>
 
+#define CHAR_SIZE (128)
 int lengthOfLongestSubstring(char* s) {
     assert(s);
     int len = strlen(s);
-    int max = len ? 1 : 0;
-    int i = 0, j = 0, k = 0;
-    int flag_break = 0;
+    int max = 0;
+    
+    if(len == 0) {
+        max = 0;
+    } else if(len == 1) {
+        max = 1;
+    } else {
+        char direct_hash[CHAR_SIZE] = {0};
+        int i = 0, j = 0;        
+        int sub_max = 0;
+        int pre_stop_pos = 0;
 
-    for(i = 0; i < len; i++)
-    {
-        for(j = i + 1; j < len; j++)
-        {
-            char key = s[j];
-            int sub_len = 0;
-            flag_break = 0;
-            for(k = i; k <= j; k++)
-            {
-                sub_len++;
-                if(s[k] == key && k != j)
-                {
-                    flag_break = 1;
+        for(i = 0; i < len; i++) {
+            int k = i ? i - 1 : 0;
+            direct_hash[(int)(s[k])] = 0;
+            //printf("%c \n", s[i]);
+            for(j = pre_stop_pos; j < len; j++) {
+               //printf("%c \n", s[j]);
+                if(direct_hash[(int)(s[j])] == 0) {
+                    direct_hash[(int)(s[j])] = 1;
+                } else {            
                     break;
                 }
-
             }
-
-            max = sub_len > max ? sub_len : max;
-            if(flag_break)
-            {
+            
+            pre_stop_pos = j;
+            sub_max = j - i;
+            //printf("max %d sub_max %d pre_stop %d  i %d \n",max, sub_max, pre_stop_pos, i);
+            max = max > sub_max ? max : sub_max;
+            if(pre_stop_pos == len) {
                 break;
             }
             
         }
-    }
+    }   
 
     return max;
-    
 }
+
 
 
 int main()
 {
-    char* test = "abcabcbb";
+    char* test = "aab";
 
     int res;
     res =  lengthOfLongestSubstring(test);
