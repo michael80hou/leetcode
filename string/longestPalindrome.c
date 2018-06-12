b926ac23d7389bff01160ce04f7301c2820b840b
michael80hou <michael80hou@gmail.com>
2018-06-12 17:26:28 +0800

    optimize for longestPalindrome, complexity O(n^2)

--- string/longestPalindrome.c
+++ string/longestPalindrome.c
@@ -3,6 +3,8 @@
 #include <assert.h>
 #include <string.h>
 
+#if 0
+//O(n^3)
 char* longestPalindrome(char* s) {
     assert(s);
     int len = strlen(s);
@@ -43,7 +45,50 @@ char* longestPalindrome(char* s) {
     return (s + maxl);
     
 }
+#else 
+//O(n^2)
+char* longestPalindrome(char* s) {
+    assert(s);
+    int len = strlen(s);    
+    char (*p)[len] = malloc(len * len);
+    assert(p);
+    memset(p, 0, len * len);
 
+    int max_diff = 0, index_l = 0, index_r = 0;
+    int l = 0, i = 0;
+    for(l = 0; l < len; l++) {
+        for(i = 0; i < len - l; i++) {
+            if(l == 0) {
+                p[i][i] = 1;
+            } else if(l == 1){
+                if(s[i] == s[i + 1]) {
+                    p[i][i + 1] = 1;
+                    if(1 > max_diff) {
+                        max_diff = 1;
+                        index_l = i;
+                        index_r = i + 1;
+                    }
+                }
+            } else {
+                if(p[i + 1][i + l - 1] && s[i] == s[i + l]) {
+                    p[i][i + l] = 1;
+                    if(l > max_diff) {
+                        max_diff = l;
+                        index_l = i;
+                        index_r = i + l;
+                    }
+                }
+            }
+        }
+    }
+
+    s[index_r + 1] = '\0';
+    return (s + index_l);
+    
+
+}
+
+#endif
 
 
 
