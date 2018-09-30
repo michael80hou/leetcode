621eee5d78334417c17de7ab462c4818ddaade24
michael80hou <michael80hou@gmail.com>
2018-09-30 10:31:13 +0800

    add dp version for edit distance

--- string/editDistance.c
+++ string/editDistance.c
@@ -3,6 +3,54 @@
 #include <assert.h>
 #include <string.h>
 
+
+#define DP_VERSION 1
+
+#ifdef DP_VERSION
+int minDistance(char* word1, char* word2) {
+
+    int res = -1;
+    int row = strlen(word1) + 1;
+    int column = strlen(word2) + 1;
+
+    int (*table)[column] = calloc(sizeof(int[column]), row);
+    if(NULL == table) {
+        printf("fatal error!\n");
+        return res;
+    }
+
+
+    for(int i = row - 1; i >= 0; i--) {
+        for(int j = column - 1 ; j >= 0; j--) {
+            if(row - 1 == i) {
+                table[i][j] = column - 1 - j;
+            } else if(column - 1 == j) {
+                table[i][j] = row - 1 - i;
+            } else {
+                if(word1[i] == word2[j]) {
+                    table[i][j] = table[i + 1][j + 1];
+                } else {
+                    int tmp = table[i + 1][j] < table[i + 1][j + 1] ? table[i + 1][j] : table[i + 1][j + 1];
+                    tmp = tmp < table[i][j + 1] ? tmp : table[i][j + 1];
+                    table[i][j] = tmp + 1;
+                }
+            }
+        }
+    }
+
+    res = table[0][0];
+    if(table) {
+        free(table);
+        table = NULL;
+    }
+
+    return res;
+    
+    
+}
+
+
+#else
 int minDistance(char* word1, char* word2) {
 
     int len1 = strlen(word1);
@@ -30,7 +78,7 @@ int minDistance(char* word1, char* word2) {
         }
     }
 }
-
+#endif
 
 
 
