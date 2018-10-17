2768a5bd20b2e14acdabb7678e31a019d51a418e
michael80hou <michael80hou@gmail.com>
2018-10-17 13:31:40 +0800

    optimize solution for longestSubstring

--- string/longestSubstring.c
+++ string/longestSubstring.c
@@ -8,54 +8,39 @@ int characterReplacement(char* s, int k) {
     assert(s);
     assert(k>=0);
 
-    int *map = malloc(sizeof(int) * 26);
+    int *map = calloc(sizeof(int), 26);
     if(NULL == map) {
         return -1;
     }
     
     int len = strlen(s);
-    if(len == 0) {
-        return 0;
-    }
-
     if(k >= len) {
         return len;
     }
 
-    int res = 0;
-    for(int l = 0; l <= len - k; l++) {
-        memset(map, 0, sizeof(int) * 26);
-        int max = 0;
-        for(int r = l; r < len; r++) {
-            int index = s[r] - 'A';
-            map[index]++;
-            max = max > map[index] ? max : map[index]; 
+    int res = 0, start = 0, max = 0;
+    for(int end = 0; end < len; end++) {
+        int tmp = ++map[s[end] - 'A'];
+        max = max > tmp ? max : tmp; 
 
-
-            int num = r - l + 1;
-            if(num - max > k) {
-                res = res > num - 1 ? res : num - 1;
-                break;
-            }
-
-            if(len - 1 == r) {
-                res = res > len - l ? res : len - l;
-            }
+        if(end - start + 1 - max > k) {
+            res = res > end - start ? res :  end - start;            
+            map[s[start] - 'A']--; 
+            start++;
         }
-
-
+        
+        res = res > end - start + 1 ? res : end - start + 1;
     }
 
-    return res;
-    
+    return res;    
 }
 
 
 int main()
 {
     int res = 0;
-    char* s = "AABABBA";
-    int k = 1;
+    char* s = "AAAA";
+    int k = 0;
 
     res =  characterReplacement(s, k);
 
