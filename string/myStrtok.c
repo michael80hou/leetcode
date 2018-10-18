c75f1769addb9ebd52517d4ee080a8b8e9644120
michael80hou <michael80hou@gmail.com>
2018-10-18 13:51:03 +0800

    add solution for strtok

--- string/myStrtok.c
+++ string/myStrtok.c
@@ -4,12 +4,40 @@
 #include <string.h>
 
 char *mystrtok(char *str, const char *delim) {
-    
 
-    return NULL;
-    
+    static int start = 0;
+    static char *ptr = NULL;
+    char *res = NULL;
+    int delim_len = strlen(delim);
 
+    if(str) {
+        start = 1;
+        ptr = str;
+        res = str;       
+    } else {
+        if(1 == start) {            
+            res = ptr;
+        } else {
+            ptr = NULL;
+            res = NULL;           
+        }
+    }
+
+    if(res) {
+        char *tmp = ptr;
+        while(strncmp(tmp, delim, delim_len) && *tmp != '\0') {
+            tmp++;
+        }
+
+        if('\0' == *tmp) {
+            start = 0;
+        } else {
+            *tmp = '\0';
+            ptr = tmp + delim_len;
+        }
+    }
     
+    return res;    
 }
 
 
@@ -17,16 +45,16 @@ char *mystrtok(char *str, const char *delim) {
 
 int main () {
    char str[80] = "This is - www.runoob.com - website";
-   const char s[2] = "-";
+   const char s[10] = "runoob";
    char *token;
    
 
-   token = strtok(str, s);
+   token = mystrtok(str, s);
 
    while( token != NULL ) {
       printf( "%s\n", token );
     
-      token = strtok(NULL, s);
+      token = mystrtok(NULL, s);
    }
    
    return(0);
