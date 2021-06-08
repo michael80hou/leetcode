79f2cbce75e4848e290d2ed8bf3da3d52ae88be2
Michael Gu <michael80hou@gmail.com>
2021-06-08 09:41:25 +0800

    add c sharp folder

---
+++ csharp/LT/LT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intput = new int[3] { 3, 2, 4 };
+            int target = 6;
+
+            Solution s = new Solution();
+
+            var res = s.TwoSum(intput, target);
+            Console.WriteLine("{0}, {1}", res[0], res[1]);
+        }
+    }
+}
---
+++ csharp/LT/LT/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LT
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> record = new Dictionary<int, int>();
+
+            int[] res = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int curValue = nums[i];
+                if (record.ContainsKey(curValue))
+                {
+                    continue;
+                }
+                else
+                {
+                    record[curValue] = i;
+
+                    int t = target - curValue;
+                    if (record.ContainsKey(t))
+                    {
+                        res[0] = i;
+                        res[1] = record[t];
+                        return res;
+                    }
+                }
+            }
+
+            return res;
+
+        }
+    }
+}
